7d8a63fb35172be11967915bed54b325403f9c65
Arkemix30 <silvacompte@gmail.com>
2022-09-04 08:55:02 -0600

    ref(db): changed environment database uir

--- app/core/config.py
+++ app/core/config.py
@@ -5,8 +5,8 @@ from app.core.loggin_config import get_logger
 logger = get_logger(__name__)
 load_dotenv(override=True)
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URI = os.environ.get("DATABASE_URI")
 
 
 def get_db_uri():
-    return DATABASE_URL
+    return DATABASE_URI
